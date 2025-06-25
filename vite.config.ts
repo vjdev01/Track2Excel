035a2a5dd1b6dc82f3327832eea6121bb14d82c4
Vijay Mutalik Desai <vmmdesai@gmail.com>
2025-06-24 22:46:28 -0700

    feat: Add gh-pages deployment support and update TypeScript linting options

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit to 1000kB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('recharts')) return 'vendor_recharts';
+            if (id.includes('react-icons')) return 'vendor_icons';
+            if (id.includes('react')) return 'vendor_react';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
